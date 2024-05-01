4cffe27a7a6ab0b250af28a18eb53cb22132db3c
GHHY47 <yanhe747@gmail.com>
2024-04-30 22:20:15 -0700

    add cloudMap for app name

---
+++ cloudMap/cloudMap/cloudMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  cloudMapApp.swift
+//  cloudMap
+//
+//  Created by He Yan on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct cloudMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
