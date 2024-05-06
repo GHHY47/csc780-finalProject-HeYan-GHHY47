2357aee2a182a321a5d7a9fe1a859f939eee259d
GHHY47 <yanhe747@gmail.com>
2024-05-06 00:35:29 -0700

    babyNoteApp

---
+++ babyNoteApp/babyNoteApp/babyNoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  babyNoteAppApp.swift
+//  babyNoteApp
+//
+//  Created by He Yan on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct babyNoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
